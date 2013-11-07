554a90c14681fb544051f33941ccf87a2c190c65
nickbosland <nickbosland@gmail.com>
2013-11-07 23:56:09 +0100

    Fix 12

--- src/nl/hsleiden/ipsen2/inf2b1/g2/views/vehicle/VehicleOverview.java
+++ src/nl/hsleiden/ipsen2/inf2b1/g2/views/vehicle/VehicleOverview.java
@@ -16,7 +16,7 @@ public class VehicleOverview extends JFrame {
 	private JPanel contentPane;
 
 	public VehicleOverview() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
 		contentPane = new JPanel();
